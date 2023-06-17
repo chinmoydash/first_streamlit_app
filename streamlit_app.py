1d0350e45b34bd021b6221e27b7d4da59289ff98
chinmoydash <chinmoy.dash@gmail.com>
2023-06-17 19:51:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit
 streamlit.title("My healthy dinner")
 
-
-streamlit.header("Breakfast Menu")
-streamlit.text("Oatmeal")
-streamlit.text("Eggs")
-streamlit.text("vegetables")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
