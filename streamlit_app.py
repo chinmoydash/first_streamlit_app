7fe9d75bee2c5cfb1592349169ec50b926928c00
chinmoydash <chinmoy.dash@gmail.com>
2023-06-19 21:11:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,9 +21,13 @@ streamlit.dataframe(fruit_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 ## asked to delete streamlit.text(fruityvice_response.json())
 
 
